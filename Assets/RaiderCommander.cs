b95961e2a1c173e64a07b55fb5a9180bec4c2dcb
John Bergsten <putte_dt@hotmail.se>
2018-08-08 23:18:15 +0200

    grass texture. Raider work. Machine gun

---
+++ Assets/RaiderCommander.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaiderCommander : MonoBehaviour
+{
+
+    public Transform m_target;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponentInChildren<BaseTurret>().M_SetTarget(m_target);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/Unit/Weapons/MachineGun.cs
+++ Assets/Scripts/Unit/Weapons/MachineGun.cs
@@ -2,20 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MachineGun : BaseWeapon
+public class MachineGun : BaseBallisticWeapon
 {
-    public float m_exitVelocity;
-    public GameObject m_projectilePrefab;
+    public float m_maxSpread; 
 
     // Use this for initialization
-    void Start()
+    protected override void Start()
     {
-
+        base.Start();
     }
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
+        base.Update();
 
+        base.Update();
+
+        // Fire
+        if (m_canFire)
+        {
+            FireWeapon();
+        }
+    }
+
+    protected override void FireWeapon()
+    {
+        base.FireWeapon();
+        GameObject newBullet = Instantiate(m_projectilePrefab);
+
+        newBullet.transform.position = transform.position;
+        newBullet.transform.rotation = transform.rotation;
+        // Create some spread
+        float spreadx = Random.Range(-m_maxSpread, m_maxSpread);
+        float spready = Random.Range(-m_maxSpread, m_maxSpread);
+        newBullet.transform.Rotate(spreadx, spready, 0, Space.Self);
+        newBullet.GetComponent<Rigidbody>().velocity = newBullet.transform.forward.normalized * m_exitVelocity;
+        Collider[] ownTankColliders = m_ownTank.GetComponentsInChildren<Collider>();
+        foreach (Collider collider in ownTankColliders)
+        {
+            Physics.IgnoreCollision(newBullet.GetComponent<Collider>(), collider);
+        }
     }
 }
