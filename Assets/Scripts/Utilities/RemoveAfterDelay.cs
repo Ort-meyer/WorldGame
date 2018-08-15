3ddf11112a087827dd69505a646992d31b6ab190
John Bergsten <putte_dt@hotmail.se>
2018-08-15 18:56:59 +0200

    generic remove after delay script. Added to machinegunbullet

---
+++ Assets/Scripts/Utilities/RemoveAfterDelay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveAfterDelay : MonoBehaviour
+{
+
+    public float m_timeToDelete;
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("DeleteSelf", m_timeToDelete);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void DeleteSelf()
+    {
+        GameObject.Destroy(this.gameObject);
+    }
+}
