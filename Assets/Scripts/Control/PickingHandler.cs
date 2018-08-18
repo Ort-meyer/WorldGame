f28dc022d0474c4388c51085476578152f47d932
John Bergsten <putte_dt@hotmail.se>
2018-08-18 12:02:53 +0200

    drag select box

--- Assets/Scripts/Control/PickingHandler.cs
+++ Assets/Scripts/Control/PickingHandler.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class PickingHandler : MonoBehaviour
 {
+
+    public static ArrayList m_currentlySelectedUnits = new ArrayList();
+    public GUIStyle m_mouseDragSkin;
+
+    private static Vector3 mouseDownPoint;
+    private static Vector3 mouseUpPoint;
+    private static Vector3 currentMousePoint;
+
+    private bool isDragging = false;
+
     public GameObject agent;
     // Use this for initialization
     void Start()
@@ -19,15 +29,47 @@ public class PickingHandler : MonoBehaviour
 
     private void HandlePick()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
         {
-            RaycastHit hit;
-            Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit))
+            currentMousePoint = hit.point;
+            if (Input.GetKeyDown(KeyCode.Mouse0))
             {
-                Vector3 positionHit = hit.point;
-                agent.GetComponent<BasicTank>().M_SetDestination(positionHit);
+                isDragging = true;
+                mouseDownPoint = hit.point;
+                {
+                    agent.GetComponent<BasicTank>().M_SetDestination(currentMousePoint);
+                }
             }
+
+            if(Input.GetKeyUp(KeyCode.Mouse0))
+            {
+                isDragging = false;
+                mouseUpPoint = Input.mousePosition;
+            }
+        } // end world hit
+        else // Didn't hit world, count as release (this should probably be handled somehow
+        {
+            isDragging = false;
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (isDragging)
+        {
+            float boxWidth = Camera.main.WorldToScreenPoint(mouseDownPoint).x - Camera.main.WorldToScreenPoint(currentMousePoint).x;
+            float boxHeight = Camera.main.WorldToScreenPoint(mouseDownPoint).y - Camera.main.WorldToScreenPoint(currentMousePoint).y;
+
+            float boxLeft = Input.mousePosition.x;
+            float boxTop = (Screen.height - Input.mousePosition.y) - boxHeight;
+
+            // Box width, height, top left corner
+            Vector2 boxPosition = new Vector2(boxLeft, boxTop);
+            Vector2 size = new Vector2(boxWidth, boxHeight);
+            GUI.Box(new Rect(boxPosition, size), "", m_mouseDragSkin);
         }
     }
 }
